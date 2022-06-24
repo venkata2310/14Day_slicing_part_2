d724799e86ad345b428b3560b8003fb9472dc35a
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-24 10:28:56 +0530

    Add files via upload

---
+++ slicing_methods.py
@@ -0,0 +1,23 @@
+a='visualworld'
+# print(a[-11:]) # visualworld
+# print(a[-10:]) # isualworld
+# print(a[-9:]) # sualworld
+# print(a[-8:]) # ualworld
+# print(a[-2:]) # ld
+# print(a[-1:]) # d
+
+# print(a[-999:]) # visualworld
+# print(a[-1:-6]) # blankline
+ 
+# print(a[::-1])
+# print(a[::-2])
+# print(a[::-3])
+# print(a[::-4])
+# print(a[::-5])
+# print(a[::-6])
+# print(a[::-7])
+# print(a[::-8])
+# print(a[::-9])
+
+# print(a[1:6:3]) #ia
+
